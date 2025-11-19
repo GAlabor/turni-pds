5f54f7649bf539cd1cff3c85ddaa90e5c8ebc9b7
galabs <g.aquilano@gmail.com>
2025-11-19 23:43:29 +0100

    3.7

--- service-worker.js
+++ service-worker.js
@@ -2,7 +2,7 @@
 // Turni PDS — Service Worker
 // ==============================
 
-const VERSION    = '2025-11-17 V3.6'; // VERSIONE APP CORRENTE
+const VERSION    = '2025-11-17 V3.7'; // VERSIONE APP CORRENTE
 const CACHE_NAME = `turni-pds-${VERSION}`;
 
 // Scope e root dinamici
