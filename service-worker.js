38b7d7ae083585fa400a7ce71f82103cb6411a5d
galabs <g.aquilano@gmail.com>
2025-12-16 23:13:51 +0100

    9.1

--- service-worker.js
+++ service-worker.js
@@ -2,7 +2,7 @@
 // Turni PdS — Service Worker
 // ==============================
 
-const VERSION    = '2025-12-15 V9.0';
+const VERSION    = '2025-12-15 V9.1';
 const CACHE_NAME = `turni-pds-${VERSION}`;
 
 const SCOPE_URL = new URL(self.registration.scope);
