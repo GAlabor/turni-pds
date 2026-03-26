46d187795ff41e3ec34cb4e00f1e5665e56d4f06
galabs <g.aquilano@gmail.com>
2026-03-26 23:33:34 +0100

    3.1.8
    Modificate icone (mancano nomi) e nomi svg esterni e nel codice

--- service-worker.js
+++ service-worker.js
@@ -1,9 +1,9 @@
-const VERSION = '3.1.7';
+const VERSION = '3.1.8';
 const APP_VERSION = VERSION;
 
 const ICO_VERSION = 'v1.0';
 const SPLASH_VERSION = 'v1.0';
-const SVG_VERSION = 'v1.1';
+const SVG_VERSION = 'v1.2';
 
 const CACHE_APP = `turni-app-${APP_VERSION}`;
 const CACHE_ICO = `turni-ico-${ICO_VERSION}`;
